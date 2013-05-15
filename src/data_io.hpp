5b65d309e23f139159875260cc6c5fe1ff659abd
Richard Barnes <rbarnes@umn.edu>
2013-05-14 23:23:33 -0500

    Moved write_arrows() into data_io.hpp so data_io.cpp could be eliminated

--- src/data_io.hpp
+++ src/data_io.hpp
@@ -410,4 +410,57 @@ int read_data(std::string filename, array2d<T> &grid){
     return load_ascii_data(filename, grid);
 }
 
+
+
+//write_arrows
+/**
+  @brief  Writes an arrow representation of a 2D D8 flow direction array
+  @author Richard Barnes (rbarnes@umn.edu)
+
+  @param[in] &filename     Name of file to write to
+  @param[in] &elevations   2D array of D8 flow directions
+
+  @returns 0 upon success
+*/
+int write_arrows(const char filename[], const char_2d &flowdirs){
+  std::locale::global(std::locale(""));
+  std::wofstream fout;
+  Timer write_time;
+  ProgressBar progress;
+
+  write_time.start();
+
+  diagnostic_arg("Opening arrow output file \"%s\"...",filename);
+  fout.open(filename);
+  if(!fout.is_open()){
+    diagnostic("failed!\n");
+    exit(-1);  //TODO: Need to make this safer! Don't just close after all that work!
+  }
+  diagnostic("succeeded.\n");
+
+  diagnostic("%%Writing arrows...\n");
+  progress.start( flowdirs.width()*flowdirs.height() );
+  for(int y=0;y<flowdirs.height();++y){
+    progress.update( y*flowdirs.width() );
+    for(int x=0;x<flowdirs.width();++x){
+      if(flowdirs(x,y)==flowdirs.no_data)  //TODO: Crude way of detecting chars and bools
+        fout<<L" ";
+      else if (flowdirs(x,y)==NO_FLOW)
+        fout<<fd[0];
+      else
+        fout<<fd[flowdirs(x,y)];
+      fout<<L" ";
+    }
+    fout<<std::endl;
+  }
+  diagnostic_arg(SUCCEEDED_IN,progress.stop());
+
+  fout.close();
+
+  write_time.stop();
+  diagnostic_arg("Write time was: %lf\n", write_time.accumulated());
+
+  return 0;
+}
+
 #endif
